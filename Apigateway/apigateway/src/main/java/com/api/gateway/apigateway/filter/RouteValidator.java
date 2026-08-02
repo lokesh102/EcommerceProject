c41419a23cbdf39017bb3fbf195625632d35db3b
Lokesh <lokeshvandavasi1405@gmail.com>
2026-08-02 08:15:42 +0530

    setting up api gateway

---
+++ Apigateway/apigateway/src/main/java/com/api/gateway/apigateway/filter/RouteValidator.java
@@ -0,0 +1,25 @@
+package com.api.gateway.apigateway.filter;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+        import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/users/register",
+            "/users/login",
+            "/users/validate",
+            "/eureka",
+            "/swagger-ui/**"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
